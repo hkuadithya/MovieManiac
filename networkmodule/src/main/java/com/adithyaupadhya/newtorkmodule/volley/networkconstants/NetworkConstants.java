af1bfd6cd2096e1c4ee337a19ee85aebce87244e
Adithya H K Upadhya <adithya.upadhya@snapdeal.com>
2016-08-18 17:03:32 +0530

    TMDB fixes, network constants

--- networkmodule/src/main/java/com/adithyaupadhya/newtorkmodule/volley/networkconstants/NetworkConstants.java
+++ networkmodule/src/main/java/com/adithyaupadhya/newtorkmodule/volley/networkconstants/NetworkConstants.java
@@ -5,7 +5,7 @@ package com.adithyaupadhya.newtorkmodule.volley.networkconstants;
  */
 public interface NetworkConstants {
     // Please obtain your own private TMDb api key through registration.
-    String API_KEY = "api_key=3db61f99f5b86b7f997e3141af909031";
+    String API_KEY = "api_key=";
 
     //  URL'S PERTAINING TO MOVIES
     String UPCOMING_MOVIES_BASE_URL = "http://api.themoviedb.org/3/movie/upcoming?" + API_KEY + "&page=";
